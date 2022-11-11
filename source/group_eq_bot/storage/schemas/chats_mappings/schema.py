619ace6d3f51824eed77b8981c8004a12caa3ead
DiSmo <di@rebels.ai>
2022-11-11 20:06:26 +0000

    feature/member-validation, added chat-mappings index

--- source/group_eq_bot/storage/schemas/chats_mappings/schema.py
+++ source/group_eq_bot/storage/schemas/chats_mappings/schema.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from elasticsearch_dsl import Date, Document, Long, Text
+from elasticsearch_dsl import Date, Document, Long, Text, Nested
 
 from interfaces.models.internal_event.event import ExpectedInternalEvent
 from storage.connectors.connector import connection
@@ -8,10 +8,13 @@ from utilities.configurations_constructor.constructor import Constructor
 CONFIGURATIONS = Constructor().configurations
 
 
-class Chat(Document):
-    chat_id = Long(required=True, )
+class Event(Document):
+    chat_id = Long(required=True)
     chat_name = Text(required=True, multi=True)
     chat_type = Text(required=True)
+
+class ChatsMappings(Document):
+    event = Nested(Event, multi=True)
     created = Date()
 
     class Index:
@@ -30,26 +33,24 @@ class Chat(Document):
 class Builder:
     def __init__(self, object: ExpectedInternalEvent):
         self.object = object
+        self.event = None
         self.schema = None
         self.index_name = None
 
+    def build_event(self):
+        self.event = Event(chat_id=abs(self.object.chat_id),
+                           chat_name=self.object.chat_name,
+                           chat_type=self.object.chat_name)
+
     def build_schema(self):
-        self.schema = Chat(chat_id=abs(self.object.chat_id),
-                           chat_name=self.object.chat_name)
+        self.schema = ChatsMappings(event=self.event)
 
     def build_index_name(self):
         self.index_name = f'{self.schema.Index.name}-chats-name-id-mappings'
 
     def build(self):
+        self.build_event()
         self.build_schema()
         self.build_index_name()
+
         return self
-
-from interfaces.telegram_event_validator.validator import EventValidator
-from tests.data.telegram_fake_events import fake_public_message_event, fake_public_member_event, fake_private_member_event, fake_private_message_event
-
-object = EventValidator(fake_public_message_event).validated_internal_event
-
-document = Builder(object=object).build()
-
-print(document.schema.save(index=document.index_name))
