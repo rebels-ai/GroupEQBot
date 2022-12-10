31d6b9a91984952e9ae11e125b17c412f46694e8
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2022-12-10 22:38:10 +0200

    test/validation, added try/except for router.

--- source/group_eq_bot/interfaces/telegram_event_router/router.py
+++ source/group_eq_bot/interfaces/telegram_event_router/router.py
@@ -17,20 +17,21 @@ async def route_event(event: TelegramEvent, context: CONTEXT_DEFAULT_TYPE) -> No
 
     logger.info('[ROUTER] New Event Registered.')
 
-    validated_event = EventValidator(external_event=event).validated_internal_event
-    logger.info('[ROUTER] New Event Validated and Casted in ExpectedInternalEvent.')
+    try:
+        validated_event = EventValidator(external_event=event).validated_internal_event
+        logger.info('[ROUTER] New Event Validated and Casted in ExpectedInternalEvent.')
 
-    chat_type = validated_event.chat_type
+        chat_type = validated_event.chat_type
 
-    if chat_type == ChatType.supergroup.value:
-        logger.info(f'[ROUTER] ChatType -- "PUBLIC.{chat_type}. Routing --> PublicEventProcessor"')
-        await PublicEventProcessor(internal_event=validated_event, context=context).handle()
+        if chat_type == ChatType.supergroup.value:
+            logger.info(f'[ROUTER] ChatType -- "PUBLIC.{chat_type}. Routing --> PublicEventProcessor"')
+            await PublicEventProcessor(internal_event=validated_event, context=context).handle()
 
-    elif chat_type == ChatType.private.value:
-        logger.info(f'[ROUTER] ChatType -- "PRIVATE.{chat_type}. Routing --> PrivateEventProcessor"')
-        await PrivateEventProcessor(internal_event=validated_event, context=context).handle()
+        elif chat_type == ChatType.private.value:
+            logger.info(f'[ROUTER] ChatType -- "PRIVATE.{chat_type}. Routing --> PrivateEventProcessor"')
+            await PrivateEventProcessor(internal_event=validated_event, context=context).handle()
 
-    else:
-        logger.warning(f'[ROUTER] ChatType -- "{chat_type}. Unexpected occurrence."')
+    except Exception as exception:
+        logger.warning(f'[ROUTER] Unexpected occurrence of {exception}."')
 
     return
